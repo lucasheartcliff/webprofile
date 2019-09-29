4aa39774bcd60bcba58463c4fd5c7c29df093a60
Lucas Morais <lucascdemorais@outlook.com>
2019-09-29 15:45:54 -0300

    Initial Commit

---
+++ src/components/button/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+    
+});
+
+export const CustomButton = (props) => {
+    return(
+        <Button
+            props
+        />
+    )
+};
