f92bea3a6ddec60a0bd9ae7b3de95efa9b69b7c9
Lucas Morais <lucascdemorais@outlook.com>
2019-09-29 20:11:21 -0300

    Setting Button Styles

---
+++ src/assets/jss/colors.js
@@ -0,0 +1,17 @@
+const royalColor = "#9c27b0";
+const warningColor = "#ff9800";
+const dangerColor = "#f44336";
+const successColor = "#4caf50";
+const infoColor = "#00acc1";
+const roseColor = "#e91e63";
+const grayColor = "#999999";
+
+export {
+    royalColor,
+    warningColor,
+    dangerColor,
+    successColor,
+    infoColor,
+    roseColor,
+    grayColor
+};
--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,15 +1,80 @@
 import React from 'react';
+import PropTypes from "prop-types";
+import classNames from "classnames";
+
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/styles';
+import buttonStyle from './buttonsStyles';
 
-const useStyles = makeStyles({
+const componentStyles = makeStyles(() => ({
+    ...buttonStyle
+}));
+
+const CustomButton = (props) => {
+    const {
+        color,
+        round,
+        children,
+        fullWidth,
+        disabled,
+        simple,
+        size,
+        block,
+        link,
+        justIcon,
+        className,
+        ...rest
+    } = props;
     
-});
+    const classes = componentStyles();
+    
+    const btnClasses = classNames({
+        [classes.button]: true,
+        [classes[size]]: size,
+        [classes[color]]: color,
+        [classes.round]: round,
+        [classes.fullWidth]: fullWidth,
+        [classes.disabled]: disabled,
+        [classes.simple]: simple,
+        [classes.block]: block,
+        [classes.link]: link,
+        [classes.justIcon]: justIcon,
+        [className]: className
+    });
 
-export const CustomButton = (props) => {
     return(
-        <Button
-            props
-        />
+        <Button {...rest} className={btnClasses}>
+            {children}
+        </Button>
     )
 };
+
+CustomButton.propTypes = {
+    color: PropTypes.oneOf([
+        "royal",
+        "info",
+        "success",
+        "warning",
+        "danger",
+        "rose",
+        "white",
+        "facebook",
+        "twitter",
+        "google",
+        "github",
+        "gitlab",
+        "transparent"
+    ]),
+    size: PropTypes.oneOf(["sm", "lg"]),
+    simple: PropTypes.bool,
+    round: PropTypes.bool,
+    fullWidth: PropTypes.bool,
+    disabled: PropTypes.bool,
+    block: PropTypes.bool,
+    link: PropTypes.bool,
+    justIcon: PropTypes.bool,
+    children: PropTypes.node,
+    className: PropTypes.string
+};
+
+export default CustomButton;
